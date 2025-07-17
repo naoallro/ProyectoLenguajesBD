4e2a1d183bf9e81a65c4736f68a845a1247e326b
naoallro <145730570+naoallro@users.noreply.github.com>
2025-07-17 06:26:49 -0600

    Navbar

---
+++ ProyectoLenguajesBD/src/main/java/com/ProyectoLenguajesBD/ProyectoLenguajesBD/ProyectoLenguajesBdApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoLenguajesBD.ProyectoLenguajesBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLenguajesBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLenguajesBdApplication.class, args);
+	}
+
+}
---
+++ ProyectoLenguajesBD/src/test/java/com/ProyectoLenguajesBD/ProyectoLenguajesBD/ProyectoLenguajesBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoLenguajesBD.ProyectoLenguajesBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLenguajesBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
