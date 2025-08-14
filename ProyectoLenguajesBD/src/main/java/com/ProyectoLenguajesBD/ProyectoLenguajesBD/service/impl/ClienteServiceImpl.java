ce54d02469c023117da4b2ec9500089f148bbf14
Sebas2506 <Sebastianflores.agui@gmail.com>
2025-08-14 17:20:43 -0600

    Update ClienteServiceImpl.java

--- ProyectoLenguajesBD/src/main/java/com/ProyectoLenguajesBD/ProyectoLenguajesBD/service/impl/ClienteServiceImpl.java
+++ ProyectoLenguajesBD/src/main/java/com/ProyectoLenguajesBD/ProyectoLenguajesBD/service/impl/ClienteServiceImpl.java
@@ -5,10 +5,32 @@
 
 package com.ProyectoLenguajesBD.ProyectoLenguajesBD.service.impl;
 
-/**
- *
- * @author naoal
- */
-public class ClienteServiceImpl {
+import com.ProyectoLenguajesBD.ProyectoLenguajesBD.dao.ClienteDao;
+import com.ProyectoLenguajesBD.ProyectoLenguajesBD.domain.Cliente;
+import com.ProyectoLenguajesBD.ProyectoLenguajesBD.service.ClienteService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteDao dao;
+    public ClienteServiceImpl(ClienteDao dao){ this.dao = dao; }
+
+    @Override @Transactional(readOnly = true)
+    public List<Cliente> getClientes(){ return dao.findAll(); }
+
+    @Override @Transactional(readOnly = true)
+    public List<Cliente> getClientesActivos(){ return dao.findByEstado("A"); }
+
+    @Override @Transactional(readOnly = true)
+    public Cliente getCliente(Long id){ return dao.findById(id).orElse(null); }
+
+    @Override @Transactional
+    public void save(Cliente c){ dao.save(c); }
+
+    @Override @Transactional
+    public void delete(Long id){ dao.deleteById(id); }
 }
+
