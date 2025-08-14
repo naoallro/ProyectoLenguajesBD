ed3540ad4c215f1e9423373985aa8a1be9ad6aa3
Sebas2506 <Sebastianflores.agui@gmail.com>
2025-08-14 17:23:59 -0600

    Update ProveedoresServiceImpl.java

--- ProyectoLenguajesBD/src/main/java/com/ProyectoLenguajesBD/ProyectoLenguajesBD/service/impl/ProveedoresServiceImpl.java
+++ ProyectoLenguajesBD/src/main/java/com/ProyectoLenguajesBD/ProyectoLenguajesBD/service/impl/ProveedoresServiceImpl.java
@@ -5,10 +5,32 @@
 
 package com.ProyectoLenguajesBD.ProyectoLenguajesBD.service.impl;
 
-/**
- *
- * @author naoal
- */
-public class ProveedoresServiceImpl {
+import com.ProyectoLenguajesBD.ProyectoLenguajesBD.dao.ProveedorDao;
+import com.ProyectoLenguajesBD.ProyectoLenguajesBD.domain.Proveedor;
+import com.ProyectoLenguajesBD.ProyectoLenguajesBD.service.ProveedorService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
+@Service
+public class ProveedoresServiceImpl implements ProveedorService {
+
+    private final ProveedorDao dao;
+    public ProveedoresServiceImpl(ProveedorDao dao){ this.dao = dao; }
+
+    @Override @Transactional(readOnly = true)
+    public List<Proveedor> getProveedores(){ return dao.findAll(); }
+
+    @Override @Transactional(readOnly = true)
+    public List<Proveedor> getProveedoresActivos(){ return dao.findByEstadoOrderByNombreAsc("A"); }
+
+    @Override @Transactional(readOnly = true)
+    public Proveedor getProveedor(Long id){ return dao.findById(id).orElse(null); }
+
+    @Override @Transactional
+    public void save(Proveedor p){ dao.save(p); }
+
+    @Override @Transactional
+    public void delete(Long id){ dao.deleteById(id); }
 }
+
